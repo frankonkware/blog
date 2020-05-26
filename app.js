295205e62f1f2b5fd3d95cf1aa31da08a0db2bde
frankonkware <frankonkware@gmail.com>
2020-05-26 13:26:57 +0300

    Initial commit

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 //npm modules
 const express = require('express');
 const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
 // create the server
 const app = express();
+
+
+app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public/')))  //tell express about static directory for putting static files like css and javascript
+
+
 
 //Homepage route
 app.get('/', (req,res) => {
@@ -12,5 +21,5 @@ app.get('/', (req,res) => {
 
 // tell the server what port to listen on
 app.listen(5000, () => {
-  console.log('Listening on localhost: ' + chalk.green('5000'))
-})
+  debug(`Listening on port: ${chalk.green('5000')}`);
+});
