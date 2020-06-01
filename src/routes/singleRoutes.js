7ab8aeece72cf70076e996a76e2f10ddcff4a331
frankonkware <frankonkware@gmail.com>
2020-06-01 17:48:42 +0300

    Add route to different file

--- src/routes/singleRoutes.js
+++ src/routes/singleRoutes.js
@@ -1,24 +1,8 @@
-// npm modules
+/* eslint-disable linebreak-style */
 const express = require('express');
-const chalk = require('chalk'); // for color coding different logs for easier analysis
-const debug = require('debug')('app');
-const morgan = require('morgan'); // for logging requests to server
-const path = require('path'); // for static dir name
 
-// create the server
-const app = express();
-const port = process.env.PORT || 5000;
+const singleRouter = express.Router();
 
-
-
-app.use(morgan('tiny')); // tiny will log minimum logs
-app.use(express.static(path.join(__dirname, '/public/'))); // tell express about static directory for putting static files like css and javascript
-app.set('views', './src/views'); // Set Views Directory
-app.set('view engine', 'ejs');
-
-const singleRouter = require('./src/routes/singleRoutes');
-
-app.use('/single', singleRouter);
 
 const blogPosts = [
   {
@@ -85,16 +69,14 @@ const blogPosts = [
     content: 'Introducing one of the most famous characters in literature, Jean Valjean—the noble peasant imprisoned for stealing a loaf of bread—Les Misérables ranks among the greatest novels of all time. In it, Victor Hugo takes readers deep into the Parisian underworld, immerses them in a battle between good and evil, and carries them to the barricades during the uprising of 1832 with a breathtaking realism that is unsurpassed in modern prose. Within his dramatic story are themes that capture the intellect and the emotions: crime and punishment, the relentless persecution of Valjean by Inspector Javert, the desperation of the prostitute Fantine, the amorality of the rogue Thénardier, and the universal desire to escape the prisons of our own minds. Les Misérables gave Victor Hugo a canvas upon which he portrayed his criticism of the French political and judicial systems, but the portrait that resulted is larger than life, epic in scope—an extravagant spectacle that dazzles the senses even as it touches the heart.',
     read: false
   }];
-// Homepage route
-app.get('/', (req, res) => {
-  res.render('index',
-    {
-      title: 'Env Blog',
-      blogPosts
-    });
-});
 
-// tell the server what port to listen on
-app.listen(port, () => {
-  debug(`Listening on port: ${chalk.green(port)}`);
-});
+// Route for single page view
+singleRouter.route('/')
+  .get((req, res) => {
+    res.render('single',
+      {
+        title: 'Env Blog: View blog'
+      });
+  });
+
+module.exports = singleRouter;
